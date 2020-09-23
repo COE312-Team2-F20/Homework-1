a68f23d4d9ed1142fb0b47e279a6e46fe8d87ca2
sarah75368 <g00075368@aus.edu>
2020-09-23 22:46:32 +0400

    added a comment

--- Homework 1/src/Main.java
+++ Homework 1/src/Main.java
@@ -9,9 +9,9 @@ class MyException extends Exception{
    }
 }
 public class Main {
-
 	public static void main(String[] args) 
     { 
+		//Introduction
 		System.out.println("Messrs Moony, Wormtail, Padfoot and Prongs Purveyors of Aids to\n" + 
 				"magical Mischief-Makers are proud to present THE MARAUDER'S MAP\n" + 
 				"\nSay the magic phrase to reveal the map.\n");
